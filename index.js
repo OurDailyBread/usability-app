f86fa6a73b358113a6bf3d6dfadc2bbbe06529a8
David Lee <david.lee@census.gov>
2016-05-11 14:42:49 -0400

    parsed values to integer again in case conversion created numbers

--- index.js
+++ index.js
@@ -318,11 +318,9 @@ app.post('/saveTouches', function(request, response) {
 
             "Name": result['name'] ? result['name'] : '',
             "Participant ID": result['pID'] ? result['pID'] : '',
-			//"Trial number": result['trialNumber'] ? result['trialNumber'] : 0,
-			"Trial number": 0,
+			"Trial number": result['trialNumber'] ? parseInt(result['trialNumber']) : 0,
 			"Stimulus number": result['combo'] ? result['combo'] : '',
-			//"Target location": result['targetLocation'] ? result['targetLocation'] : 0,
-			"Target location": 0,
+			"Target location": result['targetLocation'] ? parseInt(result['targetLocation']) : 0,
 			"Target width/diameter": result['size'] ? result['size'] : '',
 			"Target spacing": result['space'] ? result['space'] : '',
 			"Hit time ms": result['time'] ? result['time'] : '',
